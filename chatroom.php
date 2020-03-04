e8a2d61850f58dd6ed07d7c4f663104d6878c362
Atharva Deosthale <atharva.deosthale17@gmail.com>
2020-03-04 17:22:55 +0530

    Add files via upload

---
+++ chatroom.php
@@ -0,0 +1,34 @@
+<?php
+$username = $_GET["username"];
+?>
+<html>
+<head>
+    <title>Chatroom</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js"></script>
+    <script type="text/javascript">
+        var username = "<?php echo $username; ?>";
+        var io = io("3.6.233.23:8237");
+        io.emit('ident', username, function(){
+            console.log("Joined the chatroom");
+        });
+        io.on('new-msg', function(data){
+            document.getElementById("chatbox").innerHTML += "<b>" + data.by + ":</b> " + data.message + "<br>";
+        });
+        io.on('user-join', function(data){
+            document.getElementById("chatbox").innerHTML += "<b>" + data.username + "</b> joined the chat!<br>";
+        });
+        function sendmsg(){
+            var message = document.getElementById("msg").value;
+            io.emit('msg', message, function(){
+                console.log("Message sent to server!");
+            });
+            document.getElementById("msg").value = "";
+        }
+    </script>
+</head>
+<body>
+    <p id="chatbox"></p>
+    Enter your message: <input type="text" id="msg">&nbsp;
+    <button onclick="sendmsg()">Send</button>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Test Chat</title>
+    </head>
+    <body>
+        <form method="get" action="chatroom.php">
+            Enter your name: <input type="text" name="username">&nbsp;
+            <input type="submit" value="Join Chatroom">
+        </form>
+    </body>
+</html>
